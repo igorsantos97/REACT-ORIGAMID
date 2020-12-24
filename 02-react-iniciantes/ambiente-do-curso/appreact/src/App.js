8e7b76df350208c316fab7f1d55a48145dced619
Igor Santos <igornzds@gmail.com>
2020-12-24 20:40:14 -0300

    [REACT-INICIANTES] Iniciar Exercício de componentes

--- 02-react-iniciantes/ambiente-do-curso/appreact/src/App.js
+++ 02-react-iniciantes/ambiente-do-curso/appreact/src/App.js
@@ -1,49 +1,17 @@
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
+import React from 'react';
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
+
 const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
+  { nome: 'Notebook', propriedades: ['16gb ram', '512gb'] },
+  { nome: 'Smartphone', propriedades: ['2gb ram', '128gb'] },
 ];
 
 const App = () => {
-  // Origamid: "Procure fazer o filtro antes dos valores maior que 1500"
-  const dados = produtos.filter(({ preco }) =>
-    Number(preco.replace('R$ ', '') > 1500),
-  );
-
-  return (
-    <section>
-      {dados.map(({ id, nome, preco, cores }) => (
-        <div key={id}>
-          <h1>{nome}</h1>
-          <p>Preço: {preco}</p>
-          <ul>
-            {cores.map((cor) => (
-              <li key={cor} style={{ color: 'white', backgroundColor: cor }}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
-  );
+  return <section></section>;
 };
 
 export default App;

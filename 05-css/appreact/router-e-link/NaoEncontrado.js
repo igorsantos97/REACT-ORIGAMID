29f14daa433247aa55b292be49d3b23664973b5b
Igor Santos <igornzds@gmail.com>
2021-01-12 09:31:48 -0300

    [ROUTER] Finalizar as aulas de Router e Link

---
+++ 05-css/appreact/router-e-link/NaoEncontrado.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NaoEncontrado = () => {
+  return (
+    <div>
+      <p>Error: 404: Página não encontrada</p>
+    </div>
+  );
+};
+
+export default NaoEncontrado;
